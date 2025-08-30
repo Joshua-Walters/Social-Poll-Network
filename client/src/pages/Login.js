6db304765cccc5c874b552808b01ba8bd94a4894
Josh <125189307+doldrumszz@users.noreply.github.com>
2025-08-30 18:02:03 +0800

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './auth.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { toast } from 'react-toastify';
