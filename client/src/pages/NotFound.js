246bc0be3214cf5834dd56ea6cd647a7ba85f66b
Josh <125189307+doldrumszz@users.noreply.github.com>
2025-08-30 17:59:07 +0800

    Create NotFound.js

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome, FaExclamationTriangle } from 'react-icons/fa';
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <FaExclamationTriangle className="not-found-icon" />
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>The page you are looking for doesn't exist or has been moved.</p>
+        <Link to="/" className="home-link">
+          <FaHome /> Go to Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
