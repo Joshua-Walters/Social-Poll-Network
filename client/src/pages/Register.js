485dd0d79679cc23afd397db7f318055b31be2f2
Josh <125189307+doldrumszz@users.noreply.github.com>
2025-08-30 18:02:21 +0800

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { toast } from 'react-toastify';
+import './auth.css';
+
 
 const Register = () => {
   const [formData, setFormData] = useState({
